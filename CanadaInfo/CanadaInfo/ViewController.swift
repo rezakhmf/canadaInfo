e5ee0c878e2bc40e46682ad20012e8be3f8a37af
Reza Farahani <>
2020-04-24 19:59:35 +1000

    init commit

---
+++ CanadaInfo/CanadaInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanadaInfo
+//
+//  Created by Reza Farahani on 24/4/20.
+//  Copyright © 2020 farahaniconsulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
